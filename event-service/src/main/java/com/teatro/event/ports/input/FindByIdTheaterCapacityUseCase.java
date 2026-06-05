0fee0c4bd173a298465060e16d6e8f380f80c62a
Leonardo <leonardo@faccat.br>
2026-06-05 17:15:25 -0300

    Update validations and Redis configuration
    Spring Boot 4.0.6 > 3.5.14

--- event-service/src/main/java/com/teatro/event/ports/input/FindByIdTheaterCapacityUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.teatro.event.ports.input;
-
-import com.teatro.event.domain.model.Theater;
-
-public interface FindByIdTheaterCapacityUseCase {
-
-    Theater execute(Long theaterId);
-}
